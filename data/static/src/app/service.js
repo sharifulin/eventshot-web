bbffa21866c07c8a6e157c2b713c3a8261c1a9d4
Roman Dvornov <rdvornov@gmail.com>
2013-09-08 15:17:34 +0400

    remove buildin key

--- data/static/src/app/service.js
+++ data/static/src/app/service.js
@@ -3,7 +3,7 @@ basis.require('basis.net.service');
 var defaultService = new basis.net.service.Service({
   transportClass: basis.net.Transport.subclass({
     request: function(){
-      this.setParam('uuid', '9A248E954317E536A4DD1502B59BB7DB');
+      ;;;this.setParam('uuid', 'F42BA2887027AD1C9CEFC71041AD5EE6');
       basis.net.Transport.prototype.request.apply(this, arguments);
     }
   })
